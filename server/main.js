ac1ea5f03226a969287818e46b51031214520a38
Ajit Kumar <dellevenjack@gmail>
2026-02-26 21:12:14 +0530

    fix(cors): add x-auth-token to Access-Control-Allow-Headers

--- server/main.js
+++ server/main.js
@@ -29,7 +29,7 @@ async function main() {
   // allow origin https://localhost
   app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://localhost');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
     next();
   });
 
