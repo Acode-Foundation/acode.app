627928c723722c07836316f5bd94044d3f438018
Ajit Kumar <dellevenjack@gmail>
2026-05-14 14:00:21 +0530

    feat: enhance EditorType component to accept className prop and update styles in Plugins

--- client/components/editorType/index.js
+++ client/components/editorType/index.js
@@ -4,7 +4,8 @@ import './style.scss';
  * Editor type component
  * @param {object} props
  * @param {'all'|'ace'|'cm'} props.type
+ * @param {string} [props.className]
  */
-export default function EditorType({ type }) {
-  return <span className={`editor-type ${type}`} />;
+export default function EditorType({ type, className }) {
+  return <span className={`editor-type ${type} ${className ? className : ''}`.trim()} />;
 }
